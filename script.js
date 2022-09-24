5cc248e2aa5eb6e652421b69c90ad14017d83141
MadNake <72208527+MadNake@users.noreply.github.com>
2022-09-24 21:13:52 +0200

    added component and changed page rendering

--- script.js
+++ script.js
@@ -1,54 +1,79 @@
 "use stict"
 
 // создаём блоки в html
-let div = document.createElement('div');
+let div = document.getElementById('root');
 div.className = "button-block";
-document.body.append(div);
 
 
-let infoBlock = document.createElement("h1");
-infoBlock.className = "infoBlock";
+// Создаём компоненты
 
-// задаём содержимое блокам
-div.innerHTML = `<a class="button" href="#home">Home</a>
-<a class="button" href="#settings">Settings</a>
-<a class="button" href="#about">About</a>`;
+const homeComponent = `
+<a class="button" href="#home">home</a>
+<a class="button" href="#settings">settings</a>
+<a class="button" href="#about">about</a>
 
+<h1 class="info__block">Home</h1>
+`,
+	settingsComponent = `
+<a class="button" href="#home">home</a>
+<a class="button" href="#settings">settings</a>
+<a class="button" href="#about">about</a>
 
-	// infoBlock.innerHTML = `Home`;
-div.append(infoBlock);
+<h1 class="info__block">Settings</h1>
+`,
+	aboutComponent = `
+<a class="button" href="#home">home</a>
+<a class="button" href="#settings">settings</a>
+<a class="button" href="#about">about</a>
 
-// создаём массив с кнопками
-let buttons = document.getElementsByClassName("button");
+<h1 class="info__block">About</h1>
+`;
 
 
 //получаем инфу из хеша
-
 function getRouteInfo() {
 	let hash = location.hash ? location.hash.slice(1) : '';
 	return hash;
 };
 
-// меняем инфо блок в зависимости от хеша
+// Создаём функцию рендера 
+function render(content, path) {
+	path.innerHTML = content;
+}
+
+// меняем блок в зависимости от хеша
 function handleHash() {
 	let name = getRouteInfo();
-	
-	if (name) {
-		infoBlock.innerHTML = name	;
+
+	switch (name) {
+		case "home":
+			render(homeComponent, div);
+			break;
+		case "settings":
+			render(settingsComponent, div);
+			break;
+		case "about":
+			render(aboutComponent, div);
+			break;
+		default:
+			render(homeComponent, div);
+			break;
 	}
+
 };
+
+// создаём массив с кнопками
+let buttons = document.getElementsByClassName("button");
 
 //создаем обработчик событий на изменение хеша и запускаем функцию
 function init() {
-	buttons[0].click();
 	addEventListener("hashchange", handleHash);
 	handleHash();
+	buttons[0].click();
 }
 
 init();
 
-
-// задаем минимальные стили
 
 
 
